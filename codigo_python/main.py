cdd7f9fdf2b2719c0a17304b3350860c30dc0ad5
juanma1023 <juanmanuelxd3577@gmail.com>
2024-10-11 16:16:33 -0500

    Actualizacion proceso ETL

--- codigo_python/main.py
+++ codigo_python/main.py
@@ -3,4 +3,5 @@ from ETL.proceso_etl import ETLProcess
 if __name__ == "__main__":
     file_path = 'films_project\Data\Films_2.xlsx'  # Ruta al archivo Excel cargado
     etl = ETLProcess(file_path)
-    etl.run()
+    output_file = etl.run()
+print(f'Datos transformados y corregidos guardados en: {output_file}')

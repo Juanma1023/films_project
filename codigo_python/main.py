06437d543aa43863e4a9cd77b0017f00253dfb7c
juanma1023 <juanmanuelxd3577@gmail.com>
2024-10-12 09:53:17 -0500

    modificiacion del main y agregacion de comentarios

--- codigo_python/main.py
+++ codigo_python/main.py
@@ -1,7 +1,8 @@
 from ETL.proceso_etl import ETLProcess
 
+# Este bloque se ejecuta solo si el script se ejecuta directamente
 if __name__ == "__main__":
     file_path = 'films_project\Data\Films_2.xlsx'  # Ruta al archivo Excel cargado
     etl = ETLProcess(file_path)
+    # Ejecutar el proceso ETL y almacenar el resultado en output_file
     output_file = etl.run()
-print(f'Datos transformados y corregidos guardados en: {output_file}')
